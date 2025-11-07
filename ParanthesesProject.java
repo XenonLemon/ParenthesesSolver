483c3aac32078aef1a8c6f5f4667371580ab04b3
XenonLemon <pd922503@gmail.com>
2025-11-07 19:28:48 +0000

    Commit 1 11/7/25

--- ParanthesesProject.java
+++ ParanthesesProject.java
@@ -1,1 +1,32 @@
+//Peter Daniel
+//11/7/25
+//This project is meant to check if a given string has a valid set of brackets.
 
+public class ParanthesesProject{
+    public static void isValid(String s){
+        boolean parenthesesCheck = false;
+        boolean curlyCheck = false;
+        boolean squareCheck = false;
+
+        //{({()})}
+        
+        //({)}
+
+        for(int i=0; i<s.length(); i++){
+            int endCurly = s.indexOf("}");
+            int startCurly = s.indexOf("{");
+                if(startCurly < endCurly){
+                    curlyCheck = true;
+                }
+        }
+        if(curlyCheck == true){
+            System.out.println("True");
+        }
+    }
+
+    public static void main(String[] args){
+        isValid("{}");
+    }
+
+
+}
