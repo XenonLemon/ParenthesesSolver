2d9aaeb4e0e58a5af0e644b92f67d09a2d88e96e
XenonLemon <pd922503@gmail.com>
2025-11-10 19:29:41 +0000

    Commit 2 11/10/25

--- ParanthesesProject.java
+++ ParanthesesProject.java
@@ -12,20 +12,29 @@ public class ParanthesesProject{
         
         //({)}
 
-        for(int i=0; i<s.length(); i++){
-            int endCurly = s.indexOf("}");
-            int startCurly = s.indexOf("{");
-                if(startCurly < endCurly){
+        //for(int i=0; i<s.length(); i++){
+          //  int endCurly = s.indexOf("}");
+            //int startCurly = s.indexOf("{");
+              //  if(startCurly<endCurly){
+                //    curlyCheck = true;         
+                //}
+                
+        //}
+        //some kind of other loop
+        //index of before, index of after
+
+        for(int i = s.length(); i>=0; i--){
+            if(s.substring(i).equals("{")){
+                if(s.substring(i+1).equals("}")){
                     curlyCheck = true;
                 }
-        }
-        if(curlyCheck == true){
-            System.out.println("True");
+            }
+
         }
     }
 
     public static void main(String[] args){
-        isValid("{}");
+        isValid("{{}}");
     }
 
 
